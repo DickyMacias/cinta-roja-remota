af13399d5cb9a7210d1152f8e73ae88aa458a489
dicky <maciasfusco@gmail.com>
2020-03-23 21:08:15 -0700

    Primera Clase

---
+++ repaso/main.js
@@ -0,0 +1,21 @@
+
+// Tipo de dato string
+var string = "Hola Mundo";
+var stringDos = 'A';
+
+// Tipo de dato number
+var number = 49;
+var numberDos = 43.23;
+
+// Tipo de dato booleano
+var booleano = true;
+var booleanoDos = false;
+
+
+// El if solo valida booleanos
+var edad = 20;
+if (edad >= 18) {
+  console.log('Entre al if');  
+} else {
+  console.log('Entre al else');
+}
