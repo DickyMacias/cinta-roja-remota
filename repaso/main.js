be0f8fa009c3f5111c56c208322ae4e0604927a8
DickyMacias <macias@gmail.com>
2020-03-24 19:12:51 -0700

    repaso del metodo return

--- repaso/main.js
+++ repaso/main.js
@@ -25,5 +25,35 @@ for(var i = 0; i < 99; i++){
   console.log('Se ejecuto una vez ' + i);
 }
 
+//RETURN
+//El return devuelve un valor especifico de una funcion y rompe la ejecucion
+function multiplicacion(num1, num2) {
+  let result = num1 * num2;
+  return result;
+}
+
+const multi = multiplicacion(10,5);
+console.log('El resultado es: ' + multi);
 
 
+// function isPasswordValid(password){
+//   if (password === 'passwordseguro'){
+//       return 'login';
+//   } else {
+//       return 'password incorrecto';
+//   }
+// }
+
+// Esta es la definicion de la funcion
+function isPasswordValid(password){
+  if (password !== 'passwordseguro'){ // El return tambien es un break para la funcion
+    return 'password incorrecto';
+  }
+  // Doy por echo que el password es correcto
+  console.log('se esta ejecutando la funcion');
+  return 'login';
+}
+
+// Esta es la ejecucion de la funcion
+const result = isPasswordValid('mipassword');
+console.log(result);
