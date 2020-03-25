bb35feee859a4abe125de4cc904b2a66ffe03d52
DickyMacias <macias@gmail.com>
2020-03-24 20:52:07 -0700

    Ejercicio de objetos literales.

--- objetos/objetosLiterales.js
+++ objetos/objetosLiterales.js
@@ -35,6 +35,29 @@ const snoopy = {
 
 }
 
-console.log(ayudanteDeSanta.comer('carne'));
+//console.log(ayudanteDeSanta.comer('carne'));
 
 
+// 1.- Elige un pingüino de la lista de pingüinos ficticios de wikipedia 
+// 	https://en.wikipedia.org/wiki/List_of_fictional_penguins
+//     Crea un objeto llamado myPenguin con propiedades que representen
+//     la información listada en cada columna en esa página de wikipedia
+//     (por ejemplo: character, origin...)
+// 2.- Imprime el nombre del pingüino en consola, como si fuera un mensaje
+//     de bienvenida. La salida debe ser algo como:
+//     "Hola, soy un pingüino y mi nombre es [NOMBRE AQUÍ]"
+// 3- Añade un método a tu pingüino llamado 'graznar' que muestre en 
+//     consola: "kaww kaww!!"
+//     Nota: Sí, así suenan los pingüinos
+
+const myPinguin = {
+  character: 'Sparky the Wonder Penguin',
+  origin: 'This Modern World',
+  creator: 'Tom Tomorrow',
+  notes: 'A fast-talking penguin (sometimes identified as an auk) who wears a red visor and offers political commentary.',
+  graznar: () => {
+    return 'kaww kaww!!';
+  }
+}
+
+console.log("Hola, soy un pingüino y mi nombre es " + myPinguin.character + " y hago " + myPinguin.graznar());
